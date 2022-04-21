28c8a807e7e805db186436e7a145994bf791c53d
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2022-04-21 13:50:22 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+let quote = $(".quote");
+let btn = $(".generate-button");
+let cn;
+let apiKey = "WACA7lBQq6LW77SQVYu9l5HgpVC8iPjVVXsZSxd3";
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+  .then(function (response) {
+    console.log("Success! ", response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("DATA:", data);
+    quote.innerHTML = `${data.value}`;
+  })
+  .catch(function (err) {
+    console.warn("Something went wrong ", err);
+  });
+
+btn.addEventListener("click", function () {
+  location.reload();
+});
