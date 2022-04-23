9edf152ef2c5bb1d76c81a5aaaa9e24dd4acc52e
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2022-04-23 15:52:28 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const $$ = document.querySelectorAll.bind(document);
 let quote = $(".quote");
 let btn = $(".generate-button");
 let cn;
-fetch(`https://api.chucknorris.io/jokes/random`)
-  .then(function (response) {
-    console.log("Success! ", response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log("DATA:", data);
-    quote.innerHTML = `${data.value}`;
-  })
-  .catch(function (err) {
-    console.warn("Something went wrong ", err);
-  });
 
 btn.addEventListener("click", function () {
-  location.reload();
+  fetch(`https://api.chucknorris.io/jokes/random`)
+    .then(function (response) {
+      console.log("Success! ", response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("DATA:", data);
+      quote.innerHTML = `${data.value}`;
+    })
+    .catch(function (err) {
+      console.warn("Something went wrong ", err);
+    });
 });
